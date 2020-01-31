b08d4258e62c23e160390d4254d72e698cb2b35a
Jasper Kent <jasper_kent@hotmail.com>
2020-01-31 19:23:54 +0000

    Initial commit

---
+++ MVCCore/Middleware/AfterAuthentication.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace MVCCore.Middleware
+{
+    public class AfterAuthentication
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<AfterAuthentication> _logger;
+
+        public AfterAuthentication(RequestDelegate next, ILogger<AfterAuthentication> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync (HttpContext context)
+        {
+            if (context.User.Identity.IsAuthenticated)
+                _logger.LogDebug("Authenticated");
+            else
+                _logger.LogDebug("Not Authenticated");
+
+            await _next(context);
+        }
+    }
+}
---
+++ MVCFramework/Global.asax.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Diagnostics;
+
+namespace MVCFramework
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+            Trace.WriteLine(Request.IsAuthenticated ? "Authenticated" : "Not Authenticated");
+        }
+    }
+}
